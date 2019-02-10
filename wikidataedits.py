0a3073c00ef981c78b71be2e2de7812639e492ce
Vivek Maskara <maskaravivek@gmail.com>
2019-02-10 12:42:12 +0530

    Count wikidata edits through commons app

---
+++ wikidataedits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import cgi
+import sys
+from wmflabs import db
+
+#Print header
+print 'Content-type: application/json'
+
+# Fetch params
+if 'QUERY_STRING' in os.environ:
+	QS = os.environ['QUERY_STRING']
+	qs = cgi.parse_qs(QS)
+	try:
+		username = qs['user'][0].replace('_', ' ')
+	except:
+		print 'nouser'
+		sys.exit(0)
+else:
+	print 'nouser'
+	sys.exit(0)
+
+##### PROGRAM ####
+
+conn = db.connect('wikidatawiki')
+cur = conn.cursor()
+with cur:
+	sql = 'select count(*) as edit_count from change_tag join revision on rev_id=ct_rev_id where ct_tag="wikimedia-commons-app" and rev_user_text="' + username + '";'
+	cur.execute(sql)
+	data = cur.fetchall()
+
+result = data[0][0]
+print {"edits": result}
